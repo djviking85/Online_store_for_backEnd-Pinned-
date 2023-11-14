55fdaa3140d019d0d5086f89487a3973f62b004e
Viking <djviking@yandex.ru>
2023-11-14 22:00:46 +0300

    mapper exception  filter done

---
+++ src/main/java/com/example/store/exception/AdNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.store.exception;
+
+/**
+ * The AdNotFoundException class is a custom exception that is thrown when an advertisement is not found.
+ * It extends the Exception class.
+ */
+public class AdNotFoundException extends Exception{
+}
