247e2ca0fe21dbf5698825bc08d39a22fa26cee8
Viking <djviking@yandex.ru>
2023-11-14 14:52:36 +0300

    start main

---
+++ src/main/java/com/example/store/OnlineStoreProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Start project");
+		SpringApplication.run(OnlineStoreProjectApplication.class, args);
+	}
+
+}
